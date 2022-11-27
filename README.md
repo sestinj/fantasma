adc81e13073a5213cdab4065eec6dad38c56e407
Nate Sesti <33237525+sestinj@users.noreply.github.com>
2022-11-26 20:04:08 -0500

    theoretically complete

--- README.md
+++ README.md
@@ -1,23 +1,17 @@
 A stupid simple message queueing system. Basically just a place where you can funnel a bunch of events and then it will send an HTTP POST to all of the subscribers.
 There are subscriber and publisher nodes.
 
-To start a subscriber node run
+To start a node, either publisher, subscriber, or both, run
 
 ```bash
-go fantasma subscriber sub_config.json
-```
-
-To start a publisher node run
-
-```bash
-go fantasma publisher pub_config.json
+go fantasma config.json
 ```
 
 You can also edit the configs while the node is running, just edit the file and it will automatically detect changes.
 
-The `sub_config.json` file is simply a mapping between topics and processes to run. When the subscriber recieves a new message, it will look for the process to run for that topic, and pass the messages contents to an instance of the process. The message is always in JSON format and is passed to the process via a file. This means you can read the message from any programming language.
+The `config.json`.sub file is simply a mapping between topics and processes to run. When the subscriber recieves a new message, it will look for the process to run for that topic, and pass the messages contents to an instance of the process. The message is always in JSON format and is passed to the process via a file. This means you can read the message from any programming language.
 
-The `pub_config.json` file simply contains a mapping from topics to subscribers.
+The `config.json`.pub file simply contains a mapping from topics to subscribers.
 
 If a subscriber process wants to publish to a topic, it can simply make an HTTP call to the publishing node it wishes to use—this should likely be the same subscriber that called it.
 
--- main.go
+++ main.go
@@ -2,11 +2,120 @@ package main
 
 
 import (
-	"net"
+	"net/http"
 	"fmt"
 	"os"
+	"io/ioutil"
+	"encoding/json"
+	"os/exec"
+	"github.com/google/uuid"
 )
 
-func main() {
+func startServer(port string) {
+	http.ListenAndServe(":" + port, nil)
+}
+
+func handleRequest(conn net.Conn) {
+	buf := make([]byte, 1024)
+}
+
+func readConfig(path string) {
+	file, err := os.Open(path)
+	if err != nil {
+		fmt.Println("Unable to read file at " + path + ": ", err.Error())
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	bytes, _ := ioutil.ReadAll(file)
+
+	var jsonData map[string]interface{}
+	json.Unmarshal([]byte(bytes), &jsonData)
+
+	return jsonData
+}
+
+const (
+	subConfig interface{}
+	pubConfig interface{}
+)
+
+func subHandler(w http.ResponseWriter, req *http.Request) {
+	topic := req.URL.Query().Get("topic")
+
+	cmd, prs := subConfig[topic]
+	if !prs {
+		fmt.Printf(w, "y")
+		return
+	}
+
+	// Write payload to a file to pass to subprocess
+	filePath := topic+"-"+uuid.New().String()
+	err := os.WriteFile(filePath)
+	if err != nil {
+		fmt.Printf(w, "n")
+		return
+	}
 	
+	Cmd := exec.Command(cmd+" "+filePath)
+	err:= Cmd.Start()
+	if err != nil {
+		// Failure to start process, respond 'n'
+		// We only check failure to start, not to finish
+		fmt.Println("Failed to start process '" + cmd + "': ", err.Error())
+		fmt.Printf(w, "n")
+		return
+	}
+
+	go func() {
+		err := Cmd.Wait()
+		err = os.Remove(filePath)
+		if err != nil {
+			fmt.Println("Failed to delete file: ", err.Error())
+		}
+	}()
+
+	// If success, respond with 'y'
+	fmt.Printf(w, "y")
+}
+
+func pubHandler(w http.ResponseWriter, req *http.Request) {
+	var body map[string]interface{}
+	err := json.NewDecoder(req.Body).Decode(&body)
+	if err != nil {
+		fmt.Println("Failed to decode body: ", err.Error())
+		fmt.Printf(w, "n")
+		return
+	}
+	
+	ips, prs := pubConfig[req["topic"]]
+	if !prs {
+		fmt.Printf(w, "y")
+		return
+	}
+
+	jsonPayload, err := json.Marshal()
+	if err != nil {
+		fmt.Printf(w, "n")
+		return
+	}
+
+	// Send to all subscribers
+	for var ip in range ips {
+		http.Post("http://" + ip + ":3000", "application/json", jsonPayload)
+	}
+
+	fmt.Printf(w, "y")
+}
+
+func main() {
+	config = readConfig(os.Args[1])
+
+	subConfig, sprs := config["sub"]
+	pubConfig, pprs := config["pub"]
+
+	http.HandleFunc("/sub", subHandler)
+	http.HandleFunc("/pub", pubHandler)
+
+	http.ListenAndServe()
 }
